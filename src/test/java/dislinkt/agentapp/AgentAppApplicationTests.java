c23faa66e16ed2465d94313e07c8f7ea0d61fba1
ra74-2018 <nikolajovisic4@gmail.com>
2022-05-25 11:11:42 +0200

    build: init spring boot project

---
+++ src/test/java/dislinkt/agentapp/AgentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dislinkt.agentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
